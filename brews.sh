3b0f9149e09c3dc3fba0b0619e9a39da3702dec2
Kiel Oleson <kiel@jusquici.org>
2017-09-14 18:08:44 -0700

    split brewfiles between work and home - update brews script to take param

--- brews.sh
+++ brews.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 brewcmd=$( command -v brew )
+workingdir=$( pwd )
+brewsdir=$( pwd )/brew
+specifier=$1
 
 # get homebrew
 
-if [ $brewcmd == '' ]; then 
+if [ $brewcmd == '' ]; then
   echo "installing homebrew..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
@@ -12,4 +15,17 @@ else
 fi
 
 # built-in command to install from Brewfile
-brew bundle
+brew bundle --file=$brewsdir/Brewfile
+
+if [ '$1' == '' ]; then
+  echo "no brewfile specifier - stopping with base"
+  echo "specify work or home for machine-specific packages"
+elif [ $1 == 'work' ]; then
+  echo "installing work packages"
+  brew bundle --file=$brewsdir/Brewfile-work
+elif [ $1 == 'home' ]; then
+  echo "installing home packages"
+  brew bundle --file=$brewsdir/Brewfile-home
+else
+  echo "invalid location specifier - try work or home"
+fi
