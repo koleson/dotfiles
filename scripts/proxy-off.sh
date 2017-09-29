83e1a5871c89b7a5c0553dcb145ee5075ccc3814
Kiel Oleson <kiel@jusquici.org>
2017-09-28 20:57:33 -0700

    big updates to proxy on/off scripts

--- scripts/proxy-off.sh
+++ scripts/proxy-off.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+read -p "Password for network config changes: " -s SUDO_PASS
+
+echo ""
 echo "Turning off http/https proxying"
-networksetup -setwebproxystate Ethernet off
-networksetup -setsecurewebproxystate Ethernet off
+sudo networksetup -setwebproxystate Ethernet off
+sudo networksetup -setsecurewebproxystate Ethernet off
+
+MITM_PID=`cat ~/.mitmpid`
+echo "Killing mitmweb (PID $MITM_PID)"
+kill $MITM_PID
+
 echo "Done"
--- scripts/proxy-on.sh
+++ scripts/proxy-on.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+# we need sudo to do network changes
+
+read -p "Password for network config changes: " -s SUDO_PASS
+
+echo ""
 echo "Setting proxy to localhost:8080"
-networksetup -setwebproxy Ethernet 127.0.0.1 8080
-networksetup -setsecurewebproxy Ethernet 127.0.0.1 8080
+echo "$SUDO_PASS" | sudo -S networksetup -setwebproxy Ethernet 127.0.0.1 8080
+echo '$SUDO_PASS' | sudo -S networksetup -setsecurewebproxy Ethernet 127.0.0.1 8080
+
 echo "Turning on http/https proxying"
-networksetup -setwebproxystate Ethernet on
-networksetup -setsecurewebproxystate Ethernet on
+echo '$SUDO_PASS' | sudo -S networksetup -setwebproxystate Ethernet on
+echo '$SUDO_PASS' | sudo -S networksetup -setsecurewebproxystate Ethernet on
+
+echo "Starting mitmweb"
+mitmweb -q & echo $! > ~/.mitmpid
+
 echo "Done"
