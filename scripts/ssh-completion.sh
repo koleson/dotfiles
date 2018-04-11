4ee2da982d5a28177283751de5e58118ef1bb774
Kiel Oleson <kiel@jusquici.org>
2018-04-10 22:38:32 -0700

    new ssh completion

--- scripts/ssh-completion.sh
+++ scripts/ssh-completion.sh
@@ -1,4 +1,16 @@
-# https://unix.stackexchange.com/a/136356
+# https://unix.stackexchange.com/questions/136351/autocomplete-server-names-for-ssh-and-scp
 
-complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | \
-    sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
+# known_hosts is now hashed, so use config instead
+
+_ssh() 
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=$(grep '^host ' ~/.ssh/config | grep -v '[?*]' | cut -d ' ' -f 2-)
+
+    COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
+    return 0
+}
+complete -F _ssh ssh
